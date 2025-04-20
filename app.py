6070981960faa958543ca08f353a072901fa95ab
Yuganter Pratap <128248352+vansh-py04@users.noreply.github.com>
2025-04-20 17:20:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -23,7 +23,9 @@ if not os.path.exists(model_dir):
 
 # model_path = "model" # for local execution. 
 tokenizer_path = "tokenizer"
-model = AutoModelForSequenceClassification.from_pretrained(model_dir,trust_remote_code=True)
+model = AutoModelForSequenceClassification.from_pretrained(model_dir,
+                                                           trust_remote_code=True, 
+                                                           from_safetensors=True)
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 model.eval()
 
